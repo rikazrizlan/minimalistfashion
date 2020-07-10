b5d38be872bd67ab027e87e41f1d67b9bb6e61cb
Rikaz Rizlan <rikzrikaz@gmail.com>
2020-07-10 21:18:51 +0530

    Initial commit

---
+++ app.js
@@ -0,0 +1,17 @@
+const burger = document.querySelector('.burger');
+const nav = document.querySelector('.nav-links');
+const navLinks = document.querySelectorAll('.nav-links li');
+
+burger.addEventListener('click', function(){
+    nav.classList.toggle('nav-active');
+
+    navLinks.forEach( function(link, index){
+        if (link.style.animation) {
+            link.style.animation = '';
+        } else {
+            link.style.animation = `navLinkFade 0.5s ease forwards ${index/7+0.5}s`;
+        }
+    });
+    
+    burger.classList.toggle('toggle');
+});
